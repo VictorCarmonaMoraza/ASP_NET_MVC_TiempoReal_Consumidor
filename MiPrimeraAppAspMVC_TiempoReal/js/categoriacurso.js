82fb1948f18c7ce5214e3eb1719b37e34ca9be6b
Victor <VictorCarmona9035@hotmail.com>
2023-02-19 11:42:54 +0100

    6-Creando nuestro listado

--- MiPrimeraAppAspMVC_TiempoReal/js/categoriacurso.js
+++ MiPrimeraAppAspMVC_TiempoReal/js/categoriacurso.js
@@ -1,4 +1,4 @@
 ﻿//En el momento de cargar
 window.onload = function () {
-    pintar("CategoriaCurso/listarCategoriaCurso","")
+    pintar("CategoriaCurso/listarCategoriaCurso", undefined, ["Id Categoria Curso", "Nombre"], ["IdCategoriaCurso","Nombre"])
 }
--- MiPrimeraAppAspMVC_TiempoReal/js/generic.js
+++ MiPrimeraAppAspMVC_TiempoReal/js/generic.js
@@ -16,14 +16,44 @@ function setS(id, valor) {
     document.getElementById(id).src = valor;
 }
 
-function pintar(url, id) {
+function pintar(url, id="divTabla", cabeceras, nombrePropiedades) {
 
     //Hacemos la llamada
     fetch(url).then(res => res.json())
         .then(res => {
             //Esto nos devolvera solo los object 
-            alert(res)
+            //alert(res)
             //Mostrara la data obtenida en formato Json
-            alert(JSON.stringify(res))
-        })
+           // alert(JSON.stringify(res))
+
+            let contenido = "<table class='table'>";
+            contenido += "<thead>";
+            contenido += "<tr>";
+            for (const element of cabeceras) {
+                contenido += "<th>"
+                contenido += element
+                contenido += "</th>"
+            }
+            contenido += "</tr>";
+
+            contenido += "</thead>";
+            contenido += "<tbody>";
+            let objetoActual;
+            let nombrePropiedad;
+            contenido += "</tbody>";
+            for (var i = 0; i < res.length; i++) {
+                objetoActual = res[i]
+                contenido += "<tr>";
+                for (var j = 0; j < nombrePropiedades.length; j++) {
+                    nombrePropiedad = nombrePropiedades[j]
+                    contenido += "<td>"
+                    contenido += objetoActual[nombrePropiedad]
+                    contenido += "</td>"
+                }
+                contenido+="</tr>"
+            }
+            contenido += "</table>";
+            //Lo escribimos
+            document.getElementById(id).innerHTML = contenido;
+        });
 }
